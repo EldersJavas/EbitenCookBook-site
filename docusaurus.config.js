2dd8ba0bbfd1af94757c5252acb52425d4f0afe3
EldersJavas <eldersjavas@gmail.com>
2022-04-04 10:57:42 +0800

    fix🐛:  repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,13 +105,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/EldersJavas/EbitenCookBook-site/blob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/EldersJavas/EbitenCookBook-site/blob/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
